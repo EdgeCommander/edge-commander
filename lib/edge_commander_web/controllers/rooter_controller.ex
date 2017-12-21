e9be81c1f5206c2138626c580c3356660b5a2e1c
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2017-12-21 16:31:44 +0500

    Display SMS list in descending order by date. (#89)
    * Display SMS list in descending order by date.
    
    * Make function to private.

--- lib/edge_commander_web/controllers/rooter_controller.ex
+++ lib/edge_commander_web/controllers/rooter_controller.ex
@@ -72,6 +72,20 @@ defmodule EdgeCommanderWeb.RooterController do
     end
   end
 
+
+  defp get_sms(api_key, api_secret, date, to) do
+    url = "https://rest.nexmo.com/search/messages?api_key=#{api_key}&api_secret=#{api_secret}&date=#{date}&to=#{to}"
+    case HTTPoison.get(url, [], [recv_timeout: 50000]) do
+      {:ok, %{status_code: 200, body: body}} ->
+        body
+        |> Poison.decode
+        |> elem(1)
+        |> Map.get("items")
+      {:error, %{status_code: 400}} ->
+        Logger.info "No messages found"
+    end
+  end
+
   def sim_graph_and_details(conn, %{"sim_number" => sim_number} = _params) do
     api_key = System.get_env("NEXMO_API_KEY")
     api_secret =  System.get_env("NEXMO_API_SECRET")
@@ -79,41 +93,19 @@ defmodule EdgeCommanderWeb.RooterController do
     date =  Date.utc_today
     from = number_with_code(sim_number)
 
-    url = "https://rest.nexmo.com/search/messages?api_key=#{api_key}&api_secret=#{api_secret}&date=#{date}&to=#{to}"
+    incoming =
+      get_sms(api_key, api_secret, date, to)
+      |> filter_sms(from ,to)
 
-    case HTTPoison.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        sms_list =
-          body
-          |> Poison.decode
-          |> elem(1)
-          |> Map.get("items")
+    outgoing =
+      get_sms(api_key, api_secret, date, from)
+      |> filter_sms(to, from)
 
-        sms_list_1 = filter_reveived_sms(sms_list, from, to)
+    all_sms =  incoming ++ outgoing
 
-        {:error, %{status_code: 400}} ->
-          Logger.info "No messages found"
-    end
-
-
-    url = "https://rest.nexmo.com/search/messages?api_key=#{api_key}&api_secret=#{api_secret}&date=#{date}&to=#{from}"
-
-    case HTTPoison.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        sms_list =
-          body
-          |> Poison.decode
-          |> elem(1)
-          |> Map.get("items")
-
-        sms_list_2 = filter_sent_sms(sms_list, from)
-
-        {:error, %{status_code: 400}} ->
-          Logger.info "No messages found"
-    end
-
-    sms_list = sms_list_1 ++ sms_list_2
-    sms_list = Enum.sort_by(sms_list, & &1["date-received"])
+    sms_list =
+      all_sms
+      |> Enum.sort(& (&1["date-received"] >= &2["date-received"]))
 
     with %User{} <- current_user(conn) do
       render(conn, "sim_graph_and_details.html", user: current_user(conn), gravatar_url: current_user(conn) |> Map.get(:email) |> gravatar_url(secure: true), sim_number: sim_number, sms_list: sms_list)
@@ -136,14 +128,9 @@ defmodule EdgeCommanderWeb.RooterController do
     end
   end
 
-  defp filter_reveived_sms(list, from_num ,to_num) do
+  defp filter_sms(list, from_num ,to_num) do
      for list = %{ "from" => from, "to" => to } <- list,
       from == from_num &&  to == to_num , do:  list
-  end
-
-  defp filter_sent_sms(list, to_num) do
-     for list = %{ "from" => from, "to" => to } <- list,
-      to == to_num , do:  list
   end
 
   defp number_with_code("0" <> number), do: "353#{number}"
