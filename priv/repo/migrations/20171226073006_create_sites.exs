bcb16c1bdbf0ff0053779bcc08a8c4f38a039143
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2017-12-27 13:02:49 +0500

    Add Site Module. (#93)
    * Add Site Module.
    
    * Fix all changes.
    
    * Change function structure.

---
+++ priv/repo/migrations/20171226073006_create_sites.exs
@@ -0,0 +1,18 @@
+defmodule EdgeCommander.Repo.Migrations.CreateSites do
+  use Ecto.Migration
+
+  def change do
+    create table(:sites) do
+      add :name, :string
+      add :location, :string
+      add :sim_number, :string
+      add :router_id, :integer
+      add :nvr_id, :integer
+      add :notes, :string
+      add :user_id, references(:users)
+
+      timestamps()
+    end
+
+  end
+end
