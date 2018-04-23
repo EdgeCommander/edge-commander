e256d146c8560df213b43ac4abbdb9f97045d040
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2018-04-23 17:55:21 +0500

    Added Vue.js and native datatable in commands module. (#162)

--- priv/static/assets/demo/default/custom/components/datatables/base/commands_local_datatable.js
+++ priv/static/assets/demo/default/custom/components/datatables/base/commands_local_datatable.js
@@ -1,143 +1,159 @@
-var commandsDataTable = null;
-
-var DatatableDataCOMMAND = function() {
-  commandsDataTable = $(".m_commands_datatable").mDatatable({
-    data: {
-      type: "remote",
-      speedLoad: true,
-      source: "/rules",
-      pageSize: 50,
-      serverPaging: false,
-      serverFiltering: false,
-      serverSorting: false
-    },
-    layout: {
-      theme: "default",
-      class: "",
-      scroll: !1,
-      height: 950,
-      footer: !1
-    },
-    sortable: !0,
-    filterable: !1,
-    pagination: false,
-    columns: [
-    {
-      field: "actions",
-      width: 60,
-      title: "Actions",
-      locked: {left: 'xl'},
-      sortable: false,
-      template: function(t) {
-        return "<div class='editRULE cursor_to_pointer fa fa-edit' data-id='"+ t.id +"'></div> <div class='deleteRULE cursor_to_pointer fa fa-trash' data-id='"+ t.id +"'></div>";
-      },
-    },
-    {
-        field: "rule_name",
-        title: "Rule Name",
-        width: 250,
-        sortable: !1,
-        selector: !1,
-    },
-    {
-        field: "active",
-        title: "Active",
-        width: 120,
-        textAlign: "center",
-        template: function(t) {
-          if (t.active) {
-            return "Yes";
-          } else
-          {
-            return "No";
+var vm = new Vue({
+  el: '#commands_main',
+  data(){
+    return{
+       dataTable: null,
+       headings: [
+        {column: "Actions", class: "text-center"},
+        {column: "Rule Name"},
+        {column: "Active"},
+        {column: "Category"},
+        {column: "Recipients"},
+        {column: "Created At"},
+       ],
+       form_labels: {
+        name: "Rule Name",
+        category: "Category",
+        recipients: "Recipients",
+        status: "Active",
+        submit_button: "Save changes",
+        edit_title: "Edit Rule",
+        add_title: "Add Rule",
+        hide_show_title: "Show/Hide Columns",
+        add_rule_button: "Add Rule",
+        hide_show_button: "OK"
+       }
+    }
+  },
+  methods: {
+    initializeTable: function(){
+      commandsDataTable = $('#commands-datatable').DataTable({
+      ajax: {
+      url: "/rules",
+        dataSrc: function(data) {
+          return data.rules;
+        },
+        error: function(xhr, error, thrown) {
+          if (xhr.responseJSON) {
+            console.log(xhr.responseJSON.message);
+          } else {
+            console.log("Something went wrong, Please try again.");
           }
-        },
-    },
-    {
-        field: "category",
-        title: "Category",
-        textAlign: "center",
-        width: 150
-    },
-    {
-        field: "recipients",
-        title: "Recipients",
-        textAlign: "center",
-        width: 200,
-        responsive: {
-            visible: "lg"
         }
-    }, {
-        field: "created_at",
-        title: "Created At",
-        textAlign: "center",
-        template: function(t) {
-          return "" + moment(t.created_at).format('MMMM Do YYYY, H:mm:ss') +"";
+      },
+      columns: [
+      {
+        class: "text-center",
+        data: function(row, type, set, meta) {
+          return '<div class="editRULE cursor_to_pointer fa fa-edit" data-id="'+ row.id +'"></div> <div class="deleteRULE cursor_to_pointer fa fa-trash" data-id="'+ row.id +'"></div>';
+        }
+      },
+      {
+        data: function(row, type, set, meta) {
+          return row.rule_name;
+        }
+      },
+      {
+        class: "text-center",
+        data: function(row, type, set, meta) {
+          return row.active;
+        }
+      },
+      {
+        class: "text-center",
+        data: function(row, type, set, meta) {
+          return row.category;
+        }
+      },
+      {
+        data: function(row, type, set, meta) {
+          return row.recipients;
+        }
+      },
+      {
+        class: "text-center",
+        data: function(row, type, set, meta) {
+          return moment(row.created_at).format('MMMM Do YYYY, H:mm:ss');
         },
-        width: 200
+      },
+      ],
+      autoWidth: false,
+      info: false,
+      bPaginate: false,
+      lengthChange: false,
+      // stateSave:  true
+    });
+      this.dataTable = commandsDataTable;
+      this.onRuleEditButton();
+      this.deleteRULE();
+   },
+    onRuleEditButton: function(){
+    $(document).on("click", ".editRULE", function(){
+      var tr = $(this).closest('tr');
+      var row = commandsDataTable.row(tr);
+      var data = row.data();
+
+      rule_id = $(this).data("id");
+
+      $("#saveEditModal").attr('data-id', rule_id);
+      $("#edit_rule_id").val(rule_id);
+      $("#edit_rule_name").val(data.rule_name);
+      $("#edit_rule_category").val(data.category);
+      $("#edit_rule_recipients").val(data.recipients);
+      if (data.active) {
+        $('#edit_rule_is_active').prop('checked', true);
       }
-    ]
-  });
-};
-
-onSearching = function() {
-  i = commandsDataTable.getDataSourceQuery();
-  $("#m_form_search").on("keyup", function(e) {
-      commandsDataTable.search($(this).val().toLowerCase())
-  }).val(i.generalSearch)
-}
-
-var startCOMMANDTable = function() {
-  DatatableDataCOMMAND();
-};
-
-
-var onRULEButton = function() {
-  $("#addRULE").on("click", function(){
+      $('#edit_rule_to_db').modal('show');
+    });
+   },
+   showHideColumns: function(column){
+    var column = this.dataTable.column(column);
+    column.visible( ! column.visible() );
+   },
+   onRuleButton: function(){
     $('.add_rule_to_db').modal('show');
-  });
-};
-
-var discardModal = function() {
-  $("#discardModal").on("click", function() {
-    $('ul#errorOnRULE').html("")
-    clearForm();
+   },
+   clearForm: function(){
+    $("#rule_name").val("");
+    $("#rule_recipients").val("");
+    $('ul#errorOnRULE').html("");
+    $("#body-rule-dis *").prop('disabled', false);
     $("#ruleErrorDetails").addClass("hide_me");
-  });
-};
-
-var clearForm = function() {
-  $("#rule_name").val("");
-  $('ul#errorOnRULE').html("");
-  $("#set_to_load").removeClass("loading");
-  $("#body-rule-dis *").prop('disabled', false);
-  $("#ruleErrorDetails").addClass("hide_me");
-};
-
-var saveModal = function() {
-  $("#saveModal").on("click", function() {
+   },
+   sendAJAXRequest: function(settings){
+    var headers, token, xhrRequestChangeMonth;
+    token = $('meta[name="csrf-token"]');
+    if (token.length > 0) {
+    headers = {
+      "X-CSRF-Token": token.attr("content")
+    };
+    settings.headers = headers;
+    }
+    return xhrRequestChangeMonth = jQuery.ajax(settings);
+   },
+   saveModal: function(){
     $('ul#errorOnRULE').html("");
     $("#api-wait").removeClass("hide_me");
     $("#body-rule-dis *").prop('disabled',true);
     $("#ruleErrorDetails").addClass("hide_me");
-    // $("#set_to_load").addClass("loading");
+
     var rule_name     = $("#rule_name").val(),
         user_id       = $("#user_id").val(),
         category      = $('#rule_category').find(":selected").val(),
         recipients    = $("#rule_recipients").val(),
         is_active = $('input[id=rule_is_active]:checked').length > 0;
-    console.log($("#rule_category").val());
+
     var data = {};
-        if (recipients != "") {
-          data.recipients = recipients.split(",");
-        } else
-        {
-          data.recipients = "";
-        }
-        data.rule_name = rule_name;
-        data.category = category;
-        data.active = is_active;
-        data.user_id = user_id;
+    if (recipients != "") {
+      data.recipients = recipients.split(",");
+    } else
+    {
+      data.recipients = "";
+    }
+    data.rule_name = rule_name;
+    data.category = category;
+    data.active = is_active;
+    data.user_id = user_id;
 
     var settings;
 
@@ -145,162 +161,107 @@ var saveModal = function() {
       cache: false,
       data: data,
       dataType: 'json',
-      error: onError,
-      success: onSuccess,
+      error: this.onSaveError,
+      success: this.onSaveSuccess,
       contentType: "application/x-www-form-urlencoded",
       type: "POST",
       url: "/rules/new"
     };
-
-    sendAJAXRequest(settings);
-  });
-};
-
-var onError, onSuccess;
-
-onError = function(jqXHR, status, error) {
-  var cList = $('ul#errorOnRULE')
-  $.each(jqXHR.responseJSON.errors, function(index, value) {
-    var li = $('<li/>')
+    this.sendAJAXRequest(settings);
+   },
+   onSaveError: function(jqXHR, status, error) {
+      var cList = $('ul#errorOnRULE')
+      $.each(jqXHR.responseJSON.errors, function(index, value) {
+        var li = $('<li/>')
         .text(value)
         .appendTo(cList);
-  });
-  $("#ruleErrorDetails").removeClass("hide_me");
-  $("#api-wait").addClass("hide_me");
-  $("#body-rule-dis *").prop('disabled', false);
-  $("#set_to_load").removeClass("loading");
-  return false;
-};
+      });
+      $("#ruleErrorDetails").removeClass("hide_me");
+      $("#api-wait").addClass("hide_me");
+      $("#body-rule-dis *").prop('disabled', false);
+      return false;
+   },
+   onSaveSuccess: function(result, status, jqXHR) {
+    $.notify({
+      message: 'Rule has been added.'
+    },{
+      type: 'info'
+    });
+    $(".modal-backdrop").remove();
+    $("#m_modal_1").modal("hide");
+    $("#api-wait").addClass("hide_me");
+    this.dataTable.ajax.reload();
+    this.clearForm();
+    return true;
+   },
+   deleteRULE: function(){
+    $(document).on("click", ".deleteRULE", function() {
+      var ruleRow, result;
+      result = confirm("Are you sure to delete this Rule?");
+      if (result === false) {
+        return;
+      }
+      ruleRow = $(this).parents('tr');
+      ruleID = $(this).attr('data-id');
 
-onSuccess = function(result, status, jqXHR) {
-  $.notify({
-    // options
-    message: 'Rule has been added.'
-  },{
-    // settings
-    type: 'info'
-  });
-  $(".modal-backdrop").remove();
-  $("#m_modal_1").modal("hide");
-  $("#api-wait").addClass("hide_me");
-  commandsDataTable.load();
-  clearForm();
-  console.log(result);
-  return true;
-};
+      var data = {};
+      data.id = ruleID;
+      var settings;
 
-var sendAJAXRequest = function(settings) {
-  var headers, token, xhrRequestChangeMonth;
-  token = $('meta[name="csrf-token"]');
-  if (token.length > 0) {
-    headers = {
-      "X-CSRF-Token": token.attr("content")
-    };
-    settings.headers = headers;
-  }
-  return xhrRequestChangeMonth = jQuery.ajax(settings);
-};
+      settings = {
+        cache: false,
+        data: data,
+        dataType: 'json',
+        error: function(){return false},
+        success: function(){
+          this.ruleRow.remove();
+          $.notify({
+            message: 'Rule has been deleted.'
+          },{
+            type: 'info'
+          });
+          return true;
+        },
+        contentType: "application/x-www-form-urlencoded",
+        context: {ruleRow: ruleRow},
+        type: "DELETE",
+        url: "/rules/" + ruleID
+      };
 
-var deleteRULE;
-
-deleteRULE = function() {
-  $(document).on("click", ".deleteRULE", function() {
-    var ruleRow, result;
-    result = confirm("Are you sure to delete this Rule?");
-    if (result === false) {
-      return;
-    }
-    ruleRow = $(this).parents('tr');
-    ruleID = $(this).attr('data-id');
-
-    var data = {};
-    data.id = ruleID;
-    var settings;
-
-    settings = {
-      cache: false,
-      data: data,
-      dataType: 'json',
-      error: onRULEDeleteError,
-      success: onRULEDeleteSuccess,
-      contentType: "application/x-www-form-urlencoded",
-      context: {ruleRow: ruleRow},
-      type: "DELETE",
-      url: "/rules/" + ruleID
-    };
-
-    sendAJAXRequest(settings);
-  });
-};
-
-var onRULEDeleteError, onRULEDeleteSuccess;
-
-onRULEDeleteError = function(jqXHR, status, error) {
-  console.log(jqXHR.responseJSON);
-  return false;
-};
-
-onRULEDeleteSuccess = function(result, status, jqXHR) {
-  this.ruleRow.remove();
-  $.notify({
-    // options
-    message: 'Rule has been deleted.'
-  },{
-    // settings
-    type: 'info'
-  });
-  console.log(result);
-  commandsDataTable.load();
-  return true;
-};
-
-var onRULEEditButton;
-
-onRULEEditButton = function() {
-  $(document).on("click", ".editRULE", function(){
-
-    var row = $(this).closest('tr');
-    var data = commandsDataTable.jsonData[row.index()];
-    console.log(row.index())
-
-    $("#saveEditModal").attr('data-id', $(this).data("id"));
-    $("#edit_rule_name").val(data.rule_name);
-    $("#edit_rule_category").val(data.category);
-    $("#edit_rule_recipients").val(data.recipients);
-    if (data.active) {
-      $('#edit_rule_is_active').prop('checked', true);
-    }
-    $('#edit_rule_to_db').modal('show');
-  });
-};
-
-
-var updateROUTERdo;
-
-updateROUTERdo = function(){
-  $("#saveEditModal").on("click", function() {
+      var headers, token, xhrRequestChangeMonth;
+      token = $('meta[name="csrf-token"]');
+      if (token.length > 0) {
+        headers = {
+          "X-CSRF-Token": token.attr("content")
+        };
+        settings.headers = headers;
+      }
+      jQuery.ajax(settings);
+    });
+   },
+   updateRule: function(){
     $("#body-rule-edit-dis *").prop('disabled', true);
     $('ul#errorOnEditRULE').html("");
     $("#api-wait").removeClass("hide_me");
     $("#ruleEditErrorDetails").addClass("hide_me");
 
-    var ruleID = $(this).attr('data-id');
-    var rule_name     = $("#edit_rule_name").val(),
-        category      = $('#edit_rule_category').find(":selected").val(),
-        recipients    = $("#edit_rule_recipients").val(),
-        is_active = $('input[id=edit_rule_is_active]:checked').length > 0;
+    var ruleID = $("#edit_rule_id").val();
+
+    var rule_name = $("#edit_rule_name").val(),
+    category      = $('#edit_rule_category').find(":selected").val(),
+    recipients    = $("#edit_rule_recipients").val(),
+    is_active = $('input[id=edit_rule_is_active]:checked').length > 0;
 
     var data = {};
-        if (recipients != "") {
-          data.recipients = recipients.split(",");
-        } else
-        {
-          data.recipients = "";
-        }
-        data.rule_name = rule_name;
-        data.category = category;
-        data.active = is_active;
-        data.id = ruleID;
+    if (recipients != "") {
+      data.recipients = recipients.split(",");
+    }else{
+      data.recipients = "";
+    }
+    data.rule_name = rule_name;
+    data.category = category;
+    data.active = is_active;
+    data.id = ruleID;
 
     var settings;
 
@@ -308,83 +269,46 @@ updateROUTERdo = function(){
       cache: false,
       data: data,
       dataType: 'json',
-      error: onEditError,
-      success: onEditSuccess,
+      error: this.onEditError,
+      success: this.onEditSuccess,
       contentType: "application/x-www-form-urlencoded",
       type: "PATCH",
       url: "/rules/update"
     };
 
-    sendAJAXRequest(settings);
-  });
-};
+    this.sendAJAXRequest(settings);
+   },
+   onEditError: function(jqXHR, status, error) {
+    $("#api-wait").addClass("hide_me");
+    $("#body-rule-edit-dis *").prop('disabled', false);
+    var cList = $('ul#errorOnEditRULE')
+    $.each(jqXHR.responseJSON.errors, function(index, value) {
+      var li = $('<li/>')
+      .text(value)
+      .appendTo(cList);
+    });
+    $("#ruleEditErrorDetails").removeClass("hide_me");
+    return false;
+   },
+   onEditSuccess: function(result, status, jqXHR) {
+    $.notify({
+      message: 'Rule has been updated.'
+    },{
+      type: 'info'
+    });
+    $("#api-wait").addClass("hide_me");
+    this.editClearFrom();
+    $("#edit_rule_to_db").modal("hide");
+    this.dataTable.ajax.reload();
+    return true;
+   },
+   editClearFrom: function() {
+    $("#edit_rule_name").val("");
+    $('ul#errorOnEditRULE').html("");
+    $("#body-rule-edit-dis *").prop('disabled', false);
+    $("#ruleEditErrorDetails").addClass("hide_me");
+   }
+  } // end of methods
+});
 
-var onEditError, onEditSuccess;
-
-onEditError = function(jqXHR, status, error) {
-  $("#api-wait").addClass("hide_me");
-  $("#body-rule-edit-dis *").prop('disabled', false);
-  var cList = $('ul#errorOnEditRULE')
-  $.each(jqXHR.responseJSON.errors, function(index, value) {
-    var li = $('<li/>')
-        .text(value)
-        .appendTo(cList);
-  });
-  $("#ruleEditErrorDetails").removeClass("hide_me");
-  return false;
-};
-
-onEditSuccess = function(result, status, jqXHR) {
-  $.notify({
-    // options
-    message: 'Rule has been updated.'
-  },{
-    // settings
-    type: 'info'
-  });
-  $("#api-wait").addClass("hide_me");
-  editClearFrom();
-  $("#edit_rule_to_db").modal("hide");
-  commandsDataTable.load();
-  console.log(result);
-  return true;
-};
-
-var editClearFrom;
-
-editClearFrom = function() {
-  $("#edit_rule_name").val("");
-  $('ul#errorOnEditRULE').html("");
-  $("#body-rule-edit-dis *").prop('disabled', false);
-  $("#ruleEditErrorDetails").addClass("hide_me");
-}
-
-var showHideColumns;
-
-showHideColumns = function() {
-  $(".rule-column").on("click", function(){
-    console.log($(this).attr("data-field"));
-    var ColToHide = $(this).attr("data-field");
-    if(this.checked){
-      $("th[data-field='" + ColToHide + "']").show();
-      $("td[data-field='" + ColToHide + "']").show();
-    }else{
-      $("th[data-field='" + ColToHide + "']").hide();
-      $("td[data-field='" + ColToHide + "']").hide();
-    }
-    var selected_table = commandsDataTable.table;
-    $(".topScroll").width(selected_table[0].scrollWidth);
-  });
-};
-
-window.initializeCommands = function() {
-  startCOMMANDTable();
-  onSearching();
-  onRULEButton();
-  discardModal();
-  saveModal();
-  deleteRULE();
-  onRULEEditButton();
-  updateROUTERdo();
-  showHideColumns();
-};
+vm.initializeTable();
