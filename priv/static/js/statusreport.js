221bd037db329c446eabb12ed170f53f504e00f5
Ali imran <ali@evercam.io>
2018-05-09 15:42:04 +0500

    Added Vue.js in status report.

--- priv/static/js/statusreport.js
+++ priv/static/js/statusreport.js
@@ -1,100 +1,94 @@
-var initializeReport, sendAJAXRequest, nvr_logs;
+var vm = new Vue({
+  el: '#status_rpt_main',
+  data(){
+    return{}
+  },
+  methods: {
+    sendAJAXRequest: function(settings) {
+      var headers, token, xhrRequestChangeMonth;
+      token = $('meta[name="csrf-token"]');
+      if (token.length > 0) {
+        headers = {
+        "X-CSRF-Token": token.attr("content")
+        };
+        settings.headers = headers;
+      }
+      return xhrRequestChangeMonth = jQuery.ajax(settings);
+    },
+    onErrorR: function(jqXHR, status, error) {
+      return false;
+    },
+    onSuccessR: function(result, status, jqXHR) {
+      $("#api-waiting").addClass("hide_me");
+      this.startReport(result.data)
+      return true;
+    },
+    startReport: function(logs) {
+      var chart;
+      nvr_logs = logs;
+      chart = visavailChart();
+      chart.width($('.m-portlet__body').width() - 150)
+      chart.dataHeight = 10
+      $('#draw_report').text('');
+      d3.select('#draw_report').datum(logs).call(chart);
+    },
+    initializeReport: function(days) {
 
-sendAJAXRequest = function(settings) {
-  var headers, token, xhrRequestChangeMonth;
-  token = $('meta[name="csrf-token"]');
-  if (token.length > 0) {
-    headers = {
-      "X-CSRF-Token": token.attr("content")
-    };
-    settings.headers = headers;
-  }
-  return xhrRequestChangeMonth = jQuery.ajax(settings);
-};
-
-var onErrorR, onSuccessR;
-
-onErrorR = function(jqXHR, status, error) {
-  return false;
-};
-
-onSuccessR = function(result, status, jqXHR) {
-  $("#api-waiting").addClass("hide_me");
-  console.log(result);
-  startReport(result.data)
-  return true;
-};
-
-var onResize, startReport;
-
-startReport = function(logs) {
-  console.log("starting")
-  var chart;
-  nvr_logs = logs;
-  chart = visavailChart();
-  chart.width($('.m-portlet__body').width() - 150)
-  chart.dataHeight = 10
-  // chart.width($('#visavail_container').width() - 200);
-  $('#draw_report').text('');
-  d3.select('#draw_report').datum(logs).call(chart);
-};
-
-
-$(window).resize(function() {
-  startReport(nvr_logs);
-});
-
-initializeReport = function(days) {
-
-  var data = {};
+      var data = {};
       data.history_days = days
-  var settings;
+      var settings;
 
-  settings = {
-    cache: false,
-    data: data,
-    dataType: 'json',
-    error: onErrorR,
-    success: onSuccessR,
-    contentType: "application/x-www-form-urlencoded",
-    type: "GET",
-    url: "update_status_report"
-  };
+      settings = {
+        cache: false,
+        data: data,
+        dataType: 'json',
+        error: this.onErrorR,
+        success: this.onSuccessR,
+        contentType: "application/x-www-form-urlencoded",
+        type: "GET",
+        url: "update_status_report"
+      };
 
-  sendAJAXRequest(settings);
-}
+      this.sendAJAXRequest(settings);
+    },
+    initializeDate: function(){
+      $( document ).ready(function() {
+        $(".ranges ul li").on("click", function() {
+          $("#api-waiting").removeClass("hide_me");
+          clickedValues = $(this).data("range-key");
 
-$( document ).ready(function() {
-  $(".ranges ul li").on("click", function() {
-    $("#api-waiting").removeClass("hide_me");
-    clickedValues = $(this).data("range-key");
-    console.log($(this).data("range-key"));
+          var history_days;
 
-    var history_days;
+          switch (clickedValues) {
+          case "Today":
+              history_days = 1
+              break
+          case "Yesterday":
+              history_days = 2
+              break
+          case "Last 7 Days":
+              history_days = 7
+              break
+          case "Last 30 Days":
+              history_days = 30
+              break
+          default:
+              history_days = 1
+          }
 
-    switch (clickedValues) {
-    case "Today":
-        history_days = 1
-        break
-    case "Yesterday":
-        history_days = 2
-        break
-    case "Last 7 Days":
-        history_days = 7
-        break
-    case "Last 30 Days":
-        history_days = 30
-        break
-    default:
-        history_days = 1
+          vm.initializeReport(history_days);
+        });
+      });
+    },
+    resizeWindow: function(){
+      $(window).resize(function() {
+        vm.startReport(nvr_logs);
+      });
     }
-
-    console.log(history_days);
-    initializeReport(history_days);
-  });
+  }, // end of methods
+   mounted(){
+    this.initializeReport(1);
+    this.initializeDate();
+    this.resizeWindow();
+   }
 });
-
-window.initializeStatusReport = function() {
-  initializeReport(1);
-  // onSelectDate();
-};
