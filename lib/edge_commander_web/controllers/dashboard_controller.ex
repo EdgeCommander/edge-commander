e6c14ad1647cccdb0dca7e312dfd7fe776811930
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2018-05-21 16:58:58 +0500

    Changed the redirection Url if login session exist. (#192)

--- lib/edge_commander_web/controllers/dashboard_controller.ex
+++ lib/edge_commander_web/controllers/dashboard_controller.ex
@@ -6,7 +6,7 @@ defmodule EdgeCommanderWeb.DashboardController do
   def sign_up(conn, _params) do
   with %User{} <- current_user(conn) do
     conn
-    |> redirect(to: "/")
+    |> redirect(to: "/sims")
     else
       _ ->
         render(conn, "sign_up.html", csrf_token: get_csrf_token())
@@ -16,7 +16,7 @@ defmodule EdgeCommanderWeb.DashboardController do
   def sign_in(conn, _params) do
     with %User{} <- current_user(conn) do
       conn
-      |> redirect(to: "/")
+      |> redirect(to: "/sims")
     else
       _ ->
         render(conn, "sign_in.html", csrf_token: get_csrf_token())
