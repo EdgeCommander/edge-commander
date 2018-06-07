245846578d6e48b0113d35191abffd4c0c347e5d
Ali imran <ali@evercam.io>
2018-06-07 14:32:27 +0500

    Removed extra space.

--- lib/edge_commander/accounts/pipeline.ex
+++ lib/edge_commander/accounts/pipeline.ex
@@ -1,9 +1,8 @@
 defmodule EdgeCommander.Accounts.Pipeline do
 	use Guardian.Plug.Pipeline, otp_app: :edge_commander
-  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
-
+	plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
 	plug Guardian.Plug.VerifyCookie
-  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource, allow_blank: true
+	plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+	plug Guardian.Plug.EnsureAuthenticated
+	plug Guardian.Plug.LoadResource, allow_blank: true
 end
