37b3556340c705235391d5d6406c3270d60e7d43
Ali Imran <33378049+Ali7862@users.noreply.github.com>
2018-07-03 15:57:48 +0500

    Added drag and drop in data-table with save state in the browser cache. (#224)

--- priv/static/assets/demo/default/custom/components/datatables/base/sms_messages_local_datatable.js
+++ priv/static/assets/demo/default/custom/components/datatables/base/sms_messages_local_datatable.js
@@ -117,7 +117,8 @@ var vm = new Vue({
         bPaginate: false,
         lengthChange: false,
         scrollX: true,
-        // stateSave:  true,
+        colReorder: true,
+        stateSave:  true
       });
       this.dataTable = smsDataTable;
     },
@@ -279,6 +280,18 @@ var vm = new Vue({
       $('.dataTables_scrollHead').on('scroll', function () {
         $('.dataTables_scrollBody').scrollLeft($(this).scrollLeft());
       });
+    },
+    initHideShow: function(){
+      $(".sms-column").each(function(){
+        var that = $(this);
+        index = $(".sms-column").index(this);
+        status = vm.dataTable.column(index).visible();
+        if(status == 'true'){
+          $(this).prop('checked', true);
+        }else{
+          $(this).prop('checked', false);
+        }
+      });
     }
   }, // end of methods
    mounted(){
@@ -289,3 +302,4 @@ var vm = new Vue({
     window.addEventListener('resize', this.resizeScreen);
    }
 });
+vm.initHideShow();
