655a87659288d9d95abb72715c0d5504eda582b9
Ali imran <ali@evercam.io>
2018-09-24 14:35:24 +0500

    Fixed the Sim name issue (if not find any name) in messages module.

--- lib/edge_commander_web/controllers/sms_controller.ex
+++ lib/edge_commander_web/controllers/sms_controller.ex
@@ -5,6 +5,7 @@ defmodule EdgeCommanderWeb.SmsController do
   import EdgeCommander.Accounts, only: [current_user: 1]
   import EdgeCommander.ThreeScraper, only: [get_last_record_for_number: 1]
   alias EdgeCommander.Util
+  require IEx
 
   def get_all_sms(conn, params)  do
     current_user_id = Util.get_user_id(conn, params)
@@ -40,8 +41,12 @@ defmodule EdgeCommanderWeb.SmsController do
       name = "EdgeCommander"
     else
      record =  get_last_record_for_number(number)
-     name = record.name
+     name = validate_sim_name(record)
     end
     name
   end
+
+  defp validate_sim_name(nil), do: "---"
+  defp validate_sim_name(record), do: record.name
+
 end
